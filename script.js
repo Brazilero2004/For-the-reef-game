0db781eef4648e365905ae23e29708bcbd340c51
Brazilero2004 <brazilero2004@gmail.com>
2025-02-22 20:20:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ function spawnStarfish() {
     let xPosition = Math.random() * (canvas.width - starfishSize);
     starfishArray.push({ x: xPosition, y: -50, size: starfishSize, speed: starfishSpeedAdjusted });
 
-    setTimeout(spawnStarfish, adjustedSpawnRate); // Keep spawning with new rate
+    settimeout(spawnStarfish, adjustedSpawnRate); // Keep spawning with new rate
 }
 
 function updateStarfish() {
